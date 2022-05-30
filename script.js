75cf9940195ddce784254993c69607a5cb57ecaa
Boryyy <sefermahilaj123@gmail.com>
2022-05-31 00:30:26 +0200

    1.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
       $(".typed").typed({
-        strings: ["<p class='command'>maintenance</p> <p>-bash: maintenance: command not found</p> <p class='command'>well.. ^1000 this is awkward..</p><p class='command'> please contact administartor sefjon@mahilaj.com ....<span class='typed-cursor'>_</span></p>"],
+        strings: ["<p class='command'>maintenance</p> <p>-bash: maintenance: command not found</p> <p class='command'>well.. ^1000 this is awkward..</p><p class='command'> please contact administartor sefjon@mahilaj.com ....  <span class='typed-cursor'>_</span></p>"],
         typeSpeed: 0,
         loop: true,
         showCursor: false,
